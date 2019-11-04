7d334ac5d3c61d90692ff56d05ccba08d6b07f30
Yong <879048211@qq.com>
2019-11-04 17:59:47 +0800

    arrayRange

---
+++ Array/arrayRange.js
@@ -0,0 +1,20 @@
+var arrayRange = function (start, end) {
+    var arrStart = start;
+    var arrEnd = end;
+    var newArry = [];
+    if (arrStart === arrEnd || arrStart > arrEnd || typeof arrStart !== 'number') {
+        return newArry;
+    }
+    else if (typeof arrEnd !== 'number' && typeof arrEnd !== 'undefined') {
+        return newArry;
+    }
+    else if (arrEnd === undefined) {
+        arrEnd = start;
+        arrStart = 0;
+    }
+    for (var i = arrStart; arrStart <= arrEnd; arrStart++) {
+        newArry.push(arrStart);
+    }
+    return newArry;
+};
+module.exports = arrayRange;
